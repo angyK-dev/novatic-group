c6906171604464f048183d54ba6dbe2f4d0ebd33
Ange Kouakou <angemarius@gmail.com>
2021-06-02 17:20:20 +0000

    Commit after the meeting we had on 01062021
    - Ajouter un sous-menu à "A Propos"
    	LE NOM NOVATIC GROUP
    	POUR LA PETITE HISTOIRE
    	QUE FAISONS-NOUS ?
    	NOS VALEURS
    	NOTRE VISION
    	MISSION
    	NOTRE EQUIPE
    - Rendr les champs obligatoires au niveau des formes
    - Retirer la carte après la forme de contact

--- js/myscript.js
+++ js/myscript.js
@@ -8,8 +8,45 @@ Object.keys(navLinks).forEach(key => {
         // console.log(navLinks[key]);
         const _menuButton = document.querySelector("#menu-button");
         const _menu = document.querySelector("#navbarSupportedContent");
-        console.log(_menuButton.classList); 
+        // console.log(_menuButton.classList); 
         _menuButton.classList.add("collapsed");
         _menu.classList.remove("show");
     })
+});
+
+const btnContact = document.getElementById("btncontact");
+
+btnContact.addEventListener('click', () => {
+    // frmNom.textLength ? console.log(frmNom.value) : console.log("Empty");
+    let inputArr = document.getElementsByTagName("input");
+    let message = document.getElementById("formMsg");
+    // console.table(Boolean(frmNom.textLength));
+
+    // Checking the emptyness of the required fields
+    let isFilled = true;
+    for (let i = 0; i < 4; i++) {
+        const element = inputArr[i];
+        isFilled = Boolean(element.textLength);
+        if (!isFilled) {
+            // console.log(isFilled);
+            break;
+        }
+    }
+
+    // Getting the infos on the various scenarios
+    if (!isFilled) {
+        console.log("Please, fill up all the required fields.");
+    } else if (message.textLength <= 0) {
+        console.log("Please, fill up the message field.");
+    } else {
+        // Write the code to send the mail to infos@novatic-group.com
+        console.log("Write the code to send the mail to infos@novatic-group.com");
+
+        // Clearing the fields
+        for (let i = 0; i < 4; i++) {
+            const element = inputArr[i];
+            element.value = "";
+        }
+        message.value = "";
+    }
 })
