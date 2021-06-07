59c6575f640fb882fa69eb5f52264d067f042707
Ange Kouakou <angemarius@gmail.com>
2021-06-07 20:03:02 +0000

    Updated the js files
    Forgot them in the previous commit

--- js/menu.js
+++ js/menu.js
@@ -41,3 +41,18 @@
 		}
 	});
 })(jQuery);
+
+// console.log("Test");
+const navLinks = document.querySelectorAll(".nova-link");
+
+Object.keys(navLinks).forEach(key => {
+    // console.log(key, typeof navLinks[key], navLinks[key], navLinks[key].classList);
+    navLinks[key].addEventListener("click", () => {
+        // console.log(navLinks[key]);
+        const _menuButton = document.querySelector("#menu-button");
+        const _menu = document.querySelector("#navbarContent");
+        // console.log(_menuButton.classList); 
+        _menuButton.classList.add("collapsed");
+        _menu.classList.remove("show");
+    })
+});
